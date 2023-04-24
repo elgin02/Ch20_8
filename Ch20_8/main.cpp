e1c6b9651c4ffb1aeb028f0eeee885288d0289e1
Lord Elgin Raguero <elginraguero@rocketmail.com>
2023-04-24 17:12:49 -0500

    Initial Commit

---
+++ Ch20_8/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Ch20_8
+//
+//  Created by Elgin Raguero on 4/24/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
